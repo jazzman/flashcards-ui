d71f27dcb23b032f52ed20cb7c1a70a6fdcabde9
Volodymyr Lalykin <jazzman1403@gmail.com>
2022-11-01 22:56:08 +0200

    Integrate Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+import './assets/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
